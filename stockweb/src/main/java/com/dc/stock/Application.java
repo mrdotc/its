b889301262989eb15d20041f5ee79013c11c4cac
dc <mrdotch@hotmail.com>
2019-02-27 15:54:09 +0800

    Change Application Class

--- stockweb/src/main/java/com/dc/stock/Application.java
+++ stockweb/src/main/java/com/dc/stock/Application.java
@@ -15,6 +15,8 @@ public class Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SpringApplication.run(Application.class, args);
+		
+		System.out.println("log information ");
 	}
 
 }
