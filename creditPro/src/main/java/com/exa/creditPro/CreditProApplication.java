a490c449e4afaece35ca4d1baa2dddc70fb5877e
dc <mrdotch@hotmail.com>
2019-03-21 17:31:47 +0800

    commit credit proj

---
+++ creditPro/src/main/java/com/exa/creditPro/CreditProApplication.java
@@ -0,0 +1,13 @@
+package com.exa.creditPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditProApplication.class, args);
+	}
+
+}
---
+++ creditPro/src/test/java/com/exa/creditPro/CreditProApplicationTests.java
@@ -0,0 +1,16 @@
+package com.exa.creditPro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CreditProApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
